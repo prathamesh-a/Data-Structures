f7b8d81d521073e8440fb3891b480cbf200c8250
prathamesh-a <89336149+prathamesh-a@users.noreply.github.com>
2022-07-01 18:46:03 +0530

    update

--- src/me/prathamesh/LinkedList/SinglyLinkedList.java
+++ src/me/prathamesh/LinkedList/SinglyLinkedList.java
@@ -1,28 +1,54 @@
 package me.prathamesh.LinkedList;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
 
-    private Node head;
+    private Node<T> head;
 
-    private static class Node {
-        private int data;
-        private Node next;
+    private static class Node<E> {
+        private final E data;
+        private Node<E> next;
 
-        public Node(int data){
+        public Node(E data){
             this.data = data;
             this.next = null;
         }
     }
 
-    @Override
-    public String toString() {
-        Node current = head;
-        StringBuilder str = new StringBuilder();
-        while (current != null) {
-            str.append(current.data).append(" --> ");
+    /**
+     * Inserts the new node at the beginning of the linked list.
+     * The new node is created as per the given value.
+     *
+     * @param val integer value for the node to be inserted
+     * @throws NullPointerException if the specified value is null
+     */
+    public void insertFirst(T val) {
+        if (val == null) throw new NullPointerException();
+        Node<T> newNode = new Node<>(val);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public int size() {
+        if (head == null) return 0;
+        int count = 0;
+        Node<T> current = head;
+        while(current != null) {
+            count++;
             current = current.next;
         }
-        str.append("null");
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        Node<T> current = head;
+        StringBuilder str = new StringBuilder("[");
+        while (current != null) {
+            str.append(current.data).append(",");
+            current = current.next;
+        }
+        str.deleteCharAt(str.length()-1);
+        str.append("]");
         return str.toString();
     }
 }
--- src/me/prathamesh/Main.java
+++ src/me/prathamesh/Main.java
@@ -1,8 +1,16 @@
 package me.prathamesh;
 
+import me.prathamesh.LinkedList.SinglyLinkedList;
+
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        SinglyLinkedList<String > linkedList = new SinglyLinkedList<>();
+        linkedList.insertFirst("two");
+        linkedList.insertFirst("one");
+        System.out.println(linkedList);
     }
 }
