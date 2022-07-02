a17aa3104ac069b6e8531c0bdfd8ea0a43898c88
prathamesh-a <89336149+prathamesh-a@users.noreply.github.com>
2022-07-02 22:45:41 +0530

    update

--- src/me/prathamesh/Main.java
+++ src/me/prathamesh/Main.java
@@ -1,16 +1,8 @@
 package me.prathamesh;
 
-import me.prathamesh.LinkedList.SinglyLinkedList;
-
-import java.util.LinkedList;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        SinglyLinkedList<String > linkedList = new SinglyLinkedList<>();
-        linkedList.insertFirst("two");
-        linkedList.insertFirst("one");
-        System.out.println(linkedList);
     }
 }
